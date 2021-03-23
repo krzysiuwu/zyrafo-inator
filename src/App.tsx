84aa64070576d16e8d081c94e3d576f14fe964e8
MelonMusk64 <j.krzysztof13@gmail.com>
2021-03-23 15:19:13 +0100

    init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles/App.css";
+
+function App() {
+	return (
+		<div className="App">
+			<header className="App-header">hello world</header>
+		</div>
+	);
+}
+
+export default App;
